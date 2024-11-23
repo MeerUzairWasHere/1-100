945d79cceec12445841b76a6d107aff643cbb3ec
Meer Uzair <meer.uxair007@gmail.com>
2024-11-23 13:26:45 +0530

    added mocking

---
+++ 08-Testing/03-Vitest/src/index.ts
@@ -0,0 +1,60 @@
+import express from "express";
+import { z } from "zod";
+import { prismaClient } from "./db";
+import { vi } from "vitest";
+
+// Mocking the prismaClient
+vi.mock("./db", () => ({
+  prismaClient: { sum: { create: vi.fn() } },
+}));
+
+export const app = express();
+app.use(express.json());
+
+const sumInput = z.object({
+  a: z.number(),
+  b: z.number(),
+});
+
+app.post("/sum", async (req: any, res: any) => {
+  const parsedResponse = sumInput.safeParse(req.body);
+
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  await prismaClient.sum.create({
+    data: {
+      a: parsedResponse.data.a,
+      b: parsedResponse.data.b,
+      result: answer,
+    },
+  });
+
+  res.json({
+    answer,
+  });
+});
+
+app.get("/sum", (req: any, res: any) => {
+  const parsedResponse = sumInput.safeParse({
+    a: Number(req.headers["a"]),
+    b: Number(req.headers["b"]),
+  });
+
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  res.json({
+    answer,
+  });
+});
