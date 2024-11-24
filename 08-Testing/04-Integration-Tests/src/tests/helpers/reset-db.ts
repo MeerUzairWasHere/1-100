db6199bd1fe0962a95387d8cdbf1ab0f77bbc471
Meer Uzair <meer.uxair007@gmail.com>
2024-11-24 19:33:15 +0530

    added

--- 08-Testing/04-Integration-Tests/src/tests/helpers/reset-db.ts
+++ 08-Testing/04-Integration-Tests/src/tests/helpers/reset-db.ts
@@ -3,5 +3,5 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async () => {
-  await prisma.request.deleteMany();
+  await prisma.$transaction([prisma.request.deleteMany()]);
 };
